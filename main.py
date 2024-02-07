6215c2ee0762b02fe2247e0359d9bda115b4b15a
Brohelloforgem <146630422+Brohelloforgem@users.noreply.github.com>
2024-02-07 11:00:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ print("""\
 time.sleep(0.5)
 client = discord.Client(intents=discord.Intents.default())
 Token = os.getenv("TOKEN")
-Id = 1200032305067077713
+Id = 1186996547674705930
 
 
 @client.event
